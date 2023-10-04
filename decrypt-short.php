b4c2a30cbb78f8910391a283711d77567e0ae7af
Gabriel Zúñiga <gabrielz6797@gmail.com>
2023-10-04 17:55:31 -0600

    Added php code

---
+++ decrypt-short.php
@@ -0,0 +1,43 @@
+<?php
+// List of target hashcodes to match against
+$targetHashcodes = [
+    '3f179ef6da45ff638a8a72a5d96d8fb40c5478543a9052cd88cf06143ff61881',
+    'fb6f128d10e4f3388d2e989b6c2722c9b4bbaf3c822c3b0f9c0fcb8dd0518c81',
+    'fd18763c780ff93fa9c6648d92ffda3adb99ae17ae1db56ad21220ccf13d238a',
+    '0ea0cbcdeb022640b3d70e1f033555e1dc79ca051f3b58d4d8fc1bf04daba5ea',
+    '7b94d207939b521f675776a5cb2576497e4e4837cf486b788962073b92f4a644',
+    '1d43bb8159de5c25b5e16e87b262db58421bdfee1e435d1d6eb055701c72a33c',
+    '06fe01aae776142d93e5e9a658ace8cfd7b3c262568a0af0dd4b079482b217cc',
+    'f7894d2c6bed49e94564e40b7e5d52c17271ad214bc601d2c80270546b9c3540',
+    '7886f1a4d9548ccd7028254f31f2e5ce9b86b965931a5b8d62d2de1d3547e44a',
+    '1a76f4777bbb2e63189699d560063a9698e7ded82efd4f43a316acdf01b4e3ed',
+];
+
+// Load the list of words from a text file
+$wordList = file('xato-net-10-million-passwords-1000000.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+if ($wordList === false) {
+    die("Failed to open wordlist.txt");
+}
+
+// Function to compute the SHA-256 hashcode for a string
+function computeSHA256($input) {
+    return hash('sha256', $input);
+}
+
+// Loop through each word in the list
+foreach ($wordList as $word) {
+    $hash = computeSHA256($word);
+    if (in_array($hash, $targetHashcodes)) {
+        echo "Match found for hashcode $hash, word: $word" . "\n";
+    }
+
+    // Generate combinations of two words
+    foreach ($wordList as $word2) {
+        $combinedHash = computeSHA256($word . $word2);
+        if (in_array($combinedHash, $targetHashcodes)) {
+            echo "Match found for hashcode $combinedHash, words: $word, $word2" . "\n";
+        }
+    }
+}
+?>
---
+++ decrypt-test-reverse.php
@@ -0,0 +1,82 @@
+<?php
+// Start the timer
+$start = hrtime(true);
+
+// List of target hashcodes to match against
+$targetHashcodes = [
+    'e633f4fc79badea1dc5db970cf397c8248bac47cc3acf9915ba60b5d76b0e88f',
+    // Hola
+    '08361fe455add186bdecf969165380e1fcd5280cf372ef2eb9fba7f4d9c2933e',
+    // HolaPerroRojo
+    'a1db0351d0e4dce5903b4ea65add8d9d01516c4def937fe4f54a3a704cd8c2f9',
+    // AzulAzulGato
+    'a949f18a2b0bc1ca29fec75f03c5ec39f42a455a634ce1436503ebe431bce0d4',
+    // RojoRojoAdios
+    '70e732eb47b37fc6939573c708335b5d856538435a32316542a5a7e8be465939',
+    // AdiosHola
+    '996c2d41bf91598383f3e73f456b0b5ef619ce9ad69c94fea53f0570abdac852',
+    // PerroHolaGato
+    '7069e90c206b0e2d349c46d3798730577338dbb0dca9dfd2f2f897c55e668e71',
+    // Rojo
+    '05f12759e01cee642c9fd3d52eb4cd29caf15f08650a2d8bc3988ac2ac20339f',
+    // GatoRojoAzul
+    'de99e21c1000411fb9a26a5fe4be0437d30d1fcfa9bc7663d27fbc02ebf12089',
+    // GatoGato
+    '1a594301006b76e97828ccae5388b91e2feb5c654895ab1dfb7c790c57cb3d90',
+    // HolaGatoPerro
+];
+
+// Load the list of words from a text file
+$wordList = file('test.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+if ($wordList === false) {
+    die("Failed to open wordlist.txt");
+}
+
+$wordList = array_reverse($wordList);
+
+// Function to compute the SHA-256 hashcode for a string
+function computeSHA256($input)
+{
+    return hash('sha256', $input);
+}
+
+// Loop through each word in the list
+foreach ($wordList as $word) {
+    $hash = computeSHA256($word);
+    if (in_array($hash, $targetHashcodes)) {
+        echo "Match found for hashcode $hash, word: $word" . "\n"; // . "<br>";
+    }
+
+    echo $word . " " . $hash . "\n"; // . "<br>";
+
+    // Generate combinations of two words
+    foreach ($wordList as $word2) {
+        $combinedHash = computeSHA256($word . $word2);
+        if (in_array($combinedHash, $targetHashcodes)) {
+            echo "Match found for hashcode $combinedHash, words: $word, $word2" . "\n"; // . "<br>";
+        }
+
+        echo $word, $word2 . " " . $combinedHash . "\n"; // . "<br>";
+
+        // Generate combinations of three words
+        foreach ($wordList as $word3) {
+            $combinedHash = computeSHA256($word . $word2 . $word3);
+            if (in_array($combinedHash, $targetHashcodes)) {
+                echo "Match found for hashcode $combinedHash, words: $word, $word2, $word3" . "\n"; // . "<br>";
+            }
+
+            echo $word, $word2, $word3 . " " . $combinedHash . "\n"; // . "<br>";
+        }
+    }
+}
+
+// End the timer
+$end = hrtime(true);
+
+// Calculate the execution time
+$executionTime = ($end - $start) / 1e9; // Convert to seconds
+
+// Output the result
+echo "Script execution time: " . $executionTime . " seconds";
+?>
